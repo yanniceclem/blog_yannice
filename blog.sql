e637f3a4568edf9783551a0a135aa9395bc6d424
yanniceclem <clemencinyannice@gmail.com>
2025-03-11 12:00:54 +0100

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -1,1 +1,18 @@
+CREATE DATABASE blog;
 
+USE blog;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,   -- identifiant unique, avec auto-incrémentation
+    pseudo VARCHAR(50) UNIQUE,           -- pseudo de l'utilisateur
+    password VARCHAR(255),               -- mdp de l'utilisateur
+    email VARCHAR(100) UNIQUE,           -- email de l'utilisateur, unique
+    role VARCHAR(50),
+);
+
+CREATE TABLE message (
+    title VARCHAR(50),                              -- titre du msg
+    editor VARCHAR(50) UNIQUE,                      -- pseudo de l'éditeur du msg
+    image VARCHAR(255),                             -- lien de l'image 
+    date_post TIMESTAMP DEFAULT CURRENT_TIMESTAMP   -- date de création du compte
+);
