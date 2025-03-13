7bd45fa03bbd97933f83df5c7ed5538bb0a32895
yanniceclem <clemencinyannice@gmail.com>
2025-03-13 17:56:08 +0100

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -1,18 +1,24 @@
+-- Créer la base de données
 CREATE DATABASE blog;
 
+-- Utiliser la base de données
 USE blog;
 
+-- Créer la table 'user'
 CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,   -- identifiant unique, avec auto-incrémentation
-    pseudo VARCHAR(50) UNIQUE,           -- pseudo de l'utilisateur
-    password VARCHAR(255),               -- mdp de l'utilisateur
-    email VARCHAR(100) UNIQUE,           -- email de l'utilisateur, unique
-    role VARCHAR(50),
+    id INT AUTO_INCREMENT PRIMARY KEY,              -- identifiant unique
+    pseudo VARCHAR(50) UNIQUE,                      -- pseudo unique
+    password VARCHAR(255),                          -- mot de passe de l'utilisateur
+    email VARCHAR(100) UNIQUE,                      -- email unique
+    role VARCHAR(50)                                -- rôle de l'utilisateur
 );
 
+-- Créer la table 'message'
 CREATE TABLE message (
-    title VARCHAR(50),                              -- titre du msg
-    editor VARCHAR(50) UNIQUE,                      -- pseudo de l'éditeur du msg
-    image VARCHAR(255),                             -- lien de l'image 
-    date_post TIMESTAMP DEFAULT CURRENT_TIMESTAMP   -- date de création du compte
+    id INT AUTO_INCREMENT PRIMARY KEY,              -- identifiant unique pour chaque message
+    title VARCHAR(50),                              -- titre du message
+    editor_id INT,                                  -- référence à l'utilisateur (clé étrangère)
+    lien_image VARCHAR(255),                        -- lien de l'image associée au message
+    date_post TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- date de publication
+    FOREIGN KEY (editor_id) REFERENCES user(id)     -- clé étrangère pour lier le message à un utilisateur
 );
